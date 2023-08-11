ba42f4e2a0a193018ef86c89c6c998679a9dea9b
Irtiza Ashraf <irtizaashraf802@gmail.com>
2023-08-11 23:39:26 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,232 @@
+
+// QUSETION NUM 1
+
+// let number = 11;
+
+// if(number % 3 == 0){
+//       console.log("number is divisible by 3")
+
+// }
+// else{
+//       console.log("not divisible")
+// }
+
+
+// // question num 2
+// let number = prompt ();
+
+// if(number % 2 == 0){
+//       console.log("Even")
+
+// }
+// else{
+//       console.log(" Odd")
+// }
+
+// QUESTUION NUM 3
+
+
+
+// let number = prompt("Enter your age")
+
+// if (number >= 18) {
+//       console.log("Old enough")
+// }
+// else{
+//       console.log("too young");
+// }
+
+
+// question Number 4
+
+// let number = prompt(" Enter your name")
+// let myname = "ibad"
+// if (number == myname) {
+//       document.write("your name is same as mine");
+// }
+
+
+//Question num 5
+// let number = prompt("enter any number to check")
+// switch (0){
+//       case(number % 3):
+//       console.log("divisble by 3")
+//       break
+// default:console.log("not divisible")
+// }
+
+
+// //Question num 6
+
+// let entered = prompt("Enter your data")
+// if (entered===) {
+
+// }
+
+
+
+// //question num 6
+// function characterType(char) {
+//       if (char >= '0' && char <= '9') {
+//             return "Number";
+//       } else if (char >= 'A' && char <= 'Z') {
+//             return "Uppercase Letter";
+//       } else if (char >= 'a' && char <= 'z') {
+//             return "Lowercase Letter";
+//       } else {
+//             return "Unknown";
+//       }
+// }
+
+// const inputChar = prompt("Enter a character: ");
+
+// if (inputChar.length === 1) {
+//       const charType = characterType(inputChar);
+//       console.log(`The character '${inputChar}' is a ${charType}.`);
+// } else {
+//       console.log("Please enter a single character.");
+// }
+
+
+
+//question num 6
+function characterType(char) {
+      if (char >="0" && char <="9") {
+        return "Number"
+            
+      }
+      else if(char >="A" && char <= "Z"){
+         return "uppercase"
+      }
+      else if(char >= "a" && char <= "z"){
+            return "Lowercase"
+      }
+      else {
+            return
+      }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//question # 7
+
+// let number1 = +prompt("Enter your number")
+// let operator = prompt ("Enter your Operator")
+// let number2 = +prompt("Enter your numbe....")
+
+// if(operator == "+")
+// {
+// let sum =number1 +  number2;
+// console.log(sum);
+// }
+// else if(operator == "-")
+// {
+// let minus =number1 -  number2;
+// console.log(minus);
+// }
+// else if(operator == "/")
+// {
+// let div =number1 /  number2;
+// console.log(div);
+// }
+// else{
+//       console.log("not an operator")
+// }
+
+
+
+// let number1 = +prompt("Enter your number")
+// let operator = prompt("Enter your Operator")
+// let number2 = +prompt("Enter your numbe....")
+
+
+
+// switch (operator) {
+//       case "+":
+//             let sum = number1 + number2;
+//             console.log(sum)
+//             break;
+//       case "-":
+//             let sub = number1 + number2;
+//             console.log(sub)
+//             break;
+//       case "/":
+//             let divi = number1 + number2;
+//             console.log(divi)
+//             break;
+//       case "%":
+//             let per = number1 + number2;
+//             console.log(per)
+//             break;
+
+//       default: {
+//             ("invalid operator")
+//       }
+//             break;
+// }
+// document
+
+
+
+
+// if(operator == "+")
+// {
+// let sum =number1 +  number2;
+// console.log(sum);
+// }
+// else if(operator == "-")
+// {
+// let minus =number1 -  number2;
+// console.log(minus);
+// }
+// else if(operator == "/")
+// {
+// let div =number1 /  number2;
+// console.log(div);
+// }
+// else{
+//       console.log("not an operator")
+// }
+
+
+
+
+// function sumoffourno(num1, num2, num3, num4) {
+//       let sum = num1 + num2 + num3 + num4;
+//       return sum
+// }
+// sumoffourno(12, 23, 34, 45);
+// console.log(sumoffourno(12, 23, 34, 45));
+
+// console.log 
